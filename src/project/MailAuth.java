d1dbf4a6667e88da7e328abb9d8e403e5819527b
dnflskfk395 <62732915+dnflskfk395@users.noreply.github.com>
2020-05-14 17:56:30 +0900

    Update MailAuth.java

--- src/project/MailAuth.java
+++ src/project/MailAuth.java
@@ -7,8 +7,8 @@ public class MailAuth extends Authenticator{
     PasswordAuthentication pa;
 
     public MailAuth() {
-        String mail_id = "dnflskfk1994@gmail.com";
-        String mail_pw = "!y6593947";
+        String mail_id = "본인의 GMAIL";
+        String mail_pw = "본인의 GMAIL 비밀번호";
 
         pa = new PasswordAuthentication(mail_id, mail_pw);
     }
